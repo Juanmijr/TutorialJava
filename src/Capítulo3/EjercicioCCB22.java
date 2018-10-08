8f0a65915fe22cc9d880802feefd39109afeb3a8
usuario1 <usuario1@usuario>
2018-10-08 09:31:23 +0200

    k

---
+++ src/Capítulo3/EjercicioCCB22.java
@@ -0,0 +1,24 @@
+package Capítulo3;
+
+import javax.swing.JOptionPane;
+
+public class EjercicioCCB22 {
+
+	public static void main(String[] args) {
+	int numero = Integer.parseInt(JOptionPane.showInputDialog("Introduzca número"));;
+		
+		for (int i = 0; i < 101; i++) {
+			if ((numero%1) ==0){
+				System.out.println("El número " + (i) + " es múltiplo de " + numero);
+				}
+			else{
+					System.out.println("El número " + (i) + " es múltiplo de " + numero);
+				}
+			}
+		}
+	}
+
+
+	
+
+
