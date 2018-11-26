0f763a1abe008ae3507a03a1d8ea40beaca9ef7d
usuario <usuario@usuario-PC>
2018-11-26 20:36:35 +0100

    v6

--- src/capítulo4/EjercicioAhorcado/Version1/Juego.java
+++ src/capítulo4/EjercicioAhorcado/Version1/Juego.java
@@ -1,4 +1,4 @@
-package capÃ­tulo4.EjercicioAhorcado.Version1;
+package capítulo4.EjercicioAhorcado.Version1;
 
 import javax.swing.JOptionPane;
 
@@ -11,35 +11,37 @@ public class Juego {
 		int contFallos=0;
 		boolean bandera;
 		int numintentos;
-
-		//Creo bucle for para saber letra por letra las coincidencias 
-		for (int i = 0 ; i < palabra.length();i++) {
-			char coincidencias[]= new char [palabra.length()];
-			coincidencias [i] = '_';
-			System.out.print(coincidencias[i] + " ");
+		char coincidencias[]= new char [palabra.length()];
+		for(int i = 0; i<palabra.length();i++) {
+			coincidencias[i]= '_';
 		}
-		System.out.println(" ");
+		String respuesta;
 		do {
-			for (int i =0; i<6;i++) {
-				//PeticiÃ³n de respuestas
-				String respuesta = JOptionPane.showInputDialog("Introduzca la letra o palabra de la palabra");
-				//Elige si es palabra o si es letra
-				if (respuesta.length()>1){ 
-					if (respuesta.equals(palabra)) {
-						bandera = true;
-					}
-					else {
-						contFallos ++; 
-						fallos [i] = respuesta;
-					}
-				}
-				else {
-					
-				}
-				System.out.print(fallos[i]+ " ");
+			//Petición de respuestas
+			respuesta = JOptionPane.showInputDialog("Introduzca la letra o palabra de la palabra");
+			// Si la respuesta es igual que la palabra 
+			if (respuesta.equals(palabra)) {
+				bandera = true;
 			}
-		}while (bandera=false);
-		System.out.println("Enhorabuena has acertado");
+			else {
+				if (respuesta.length()==1) {
+					for (int i = 0; i<palabra.length();i++) {
+						char decision = respuesta.charAt(0);
+						char letraPalabra = palabra.charAt(i);
+						System.out.print(coincidencias[i] + " ");
+						if (letraPalabra==decision) {
+							coincidencias[i]=decision;
+							System.out.print(" " + coincidencias[i] + " ");
+						}
+						else {
+							
+						}	
+						}
+				}
+			}
+//
+		}while (bandera=true);
+		System.out.println("\n\nEnhorabuena has acertado");
 
 
 }
